684d257c39efaa7bb420f2363893d753f1ca9a15
Tirso Lopez <tirso.lopez@multiversecomputing.com>
2025-12-19 19:20:29 +0100

    Enable Snacks explorer and configure Neo-tree backup
     • Enabled kickstart.plugins.neo-tree in init.lua.
     • Added explorer module to snacks.lua with keybindings.
     • Configured Snacks explorer with dashboard and file actions.
     • Set up auto-close for dashboard buffers on file open.
     • Configured Neo-tree as a backup explorer with new keybindings.
     • Updated Neo-tree settings for window position and file system.
     • Added file filtering and git status symbols in Neo-tree.

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -3,6 +3,8 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
+    -- Enable explorer module
+    explorer = { enabled = true },
     dashboard = {
       preset = {
         header = [[
@@ -16,6 +18,7 @@ return {
 ██████  █████████████████████ ████ █████ █████ ████ ██████
         ]],
         keys = {
+          { icon = ' ', key = 'e', desc = 'Explorer', action = function() Snacks.explorer() end },
           { icon = ' ', key = 'f', desc = 'Find File', action = ':Telescope find_files' },
           { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
           { icon = ' ', key = 'g', desc = 'Find Text', action = ':Telescope live_grep' },
@@ -25,6 +28,9 @@ return {
         },
       },
     },
+  },
+  keys = {
+    { '<leader>e', function() Snacks.explorer() end, desc = 'Explorer (Snacks)' },
   },
   config = function(_, opts)
     require('snacks').setup(opts)
@@ -36,6 +42,27 @@ return {
         if arg ~= '' and vim.fn.isdirectory(arg) == 1 then
           vim.cmd('bdelete')
           require('snacks').dashboard()
+        end
+      end,
+    })
+
+    -- Close dashboard when opening a file
+    vim.api.nvim_create_autocmd('BufEnter', {
+      callback = function()
+        local buftype = vim.bo.buftype
+        local filetype = vim.bo.filetype
+        -- Skip special buffers (dashboard, explorer, etc.)
+        if buftype ~= '' or filetype == 'snacks_dashboard' or filetype == 'snacks_picker_list' then
+          return
+        end
+        -- Close any dashboard buffers when entering a real file
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+          if vim.api.nvim_buf_is_valid(buf) then
+            local ft = vim.bo[buf].filetype
+            if ft == 'snacks_dashboard' then
+              vim.api.nvim_buf_delete(buf, { force = true })
+            end
+          end
         end
       end,
     })
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,23 +1,48 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
+-- Configured as BACKUP explorer (primary is Snacks.explorer)
 
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>E', ':Neotree toggle<CR>', desc = 'Explorer (Neo-tree)', silent = true },
   },
   opts = {
+    close_if_last_window = true,
+    window = {
+      position = 'left',
+      width = 30,
+      mappings = {
+        ['<leader>E'] = 'close_window',
+      },
+    },
     filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+      hijack_netrw_behavior = 'disabled',
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_by_name = { '.git', 'node_modules' },
+        never_show = { '.DS_Store' },
+      },
+    },
+    default_component_configs = {
+      git_status = {
+        symbols = {
+          added = '+',
+          modified = '~',
+          deleted = '-',
+          untracked = '?',
+          ignored = '◌',
+          unstaged = '✗',
+          staged = '✓',
         },
       },
     },
